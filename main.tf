6ca5cf7aaa54494d368d26a5e24ab3c707d17638
pstephenson02 <phil.m.stepenson@datadoghq.com>
2023-01-25 11:28:06 -0800

    Complete steps

--- main.tf
+++ main.tf
@@ -171,4 +171,113 @@ resource "datadog_synthetics_test" "book_court" {
         })
     }
   }
+
+  browser_step {
+    name = "Click on last day in schedule"
+    type = "click"
+    params {
+      element = jsonencode({
+            "url": "https://wam.playbypoint.com/book/wam",
+            "targetOuterHTML": "<button class=\"ui button selectable basic\"><div class=\"day_name\">Tue</div><div class=\"day_number\">31</div></button>"
+            "multiLocator": {
+              "ab": "/*[local-name()=\"html\"][1]/*[local-name()=\"body\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][2]/*[local-name()=\"table\"][1]/*[local-name()=\"tbody\"][1]/*[local-name()=\"tr\"][1]/*[local-name()=\"td\"][2]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"button\"][8]",
+              "co": "[{\"relation\":\"PARENT OF\",\"tagName\":\"button\",\"text\":\"tue31\",\"textType\":\"innerText\"}]",
+              "cl": "/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" more_height \")]/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" flex_mobile_button_container \")]/*[local-name()=\"button\"][8]",
+              "at": "",
+              "clt": "/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" more_height \")]/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" flex_mobile_button_container \")]/*[local-name()=\"button\"][8]",
+              "ro": "//*[1]/*[local-name()=\"div\"][1]/*[local-name()=\"button\"][8]"
+            }
+        })
+      element_user_locator {
+        fail_test_on_cannot_locate = true
+        value {
+            type = "css"
+            value = "{{ LAST_DAY_SELECTOR }}"
+        }
+      }
+    }
+  }
+
+  browser_step {
+    name = "Click on preferred court time"
+    type = "click"
+    params {
+      element = jsonencode({
+            "url": "https://wam.playbypoint.com/book/wam",
+            "targetOuterHTML": "<button class=\"ButtonOption ui button basic  with_group firstOfGroup\">6:30-7:45am </button>"
+            "multiLocator": {
+              "ab": "/*[local-name()=\"html\"][1]/*[local-name()=\"body\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][2]/*[local-name()=\"table\"][1]/*[local-name()=\"tbody\"][1]/*[local-name()=\"tr\"][1]/*[local-name()=\"td\"][2]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][4]/*[local-name()=\"div\"][2]/*[local-name()=\"button\"][1]",
+              "co": "[{\"text\":\"6:30-7:45am\",\"textType\":\"directText\"}]",
+              "cl": "/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" hours_list \")]/*[local-name()=\"button\"][1]",
+              "at": "",
+              "clt": "/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" hours_list \")]/descendant::*[text()[normalize-space(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞŸŽŠŒ', 'abcdefghijklmnopqrstuvwxyzàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿžšœ')) = \"6:30-7:45am\"]]",
+              "ro": "//*[text()[normalize-space(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞŸŽŠŒ', 'abcdefghijklmnopqrstuvwxyzàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿžšœ')) = \"6:30-7:45am\"]]"
+            }
+        })
+      element_user_locator {
+        fail_test_on_cannot_locate = true
+        value {
+            type = "xpath"
+            value = "{{ COURT_TIME_545_7PM_XPATH_SELECTOR }}"
+        }
+      }
+    }
+  }
+
+  browser_step {
+    name = "Click on button \"Next\""
+    type = "click"
+    params {
+      element = jsonencode({
+            "url": "https://wam.playbypoint.com/book/wam",
+            "targetOuterHTML": "<button class=\"ui icon small button primary \"><span class=\"\"> Next <i class=\"icon icon-arrow-right\"></i></span></button>",
+            "multiLocator": {
+              "ab": "/*[local-name()=\"html\"][1]/*[local-name()=\"body\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][2]/*[local-name()=\"table\"][1]/*[local-name()=\"tbody\"][1]/*[local-name()=\"tr\"][1]/*[local-name()=\"td\"][2]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][2]/*[local-name()=\"button\"][1]",
+              "co": "[{\"relation\":\"PARENT OF\",\"tagName\":\"button\",\"text\":\" next \",\"textType\":\"innerText\"},{\"relation\":\"PARENT OF\",\"tagName\":\"DIV\",\"text\":\"select date and timetuesday, jan 31, 6:30 am - 7:45 am\",\"textType\":\"innerText\"}]",
+              "cl": "/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" Stepper \")]/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" buttons \")][1]/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" button \")]",
+              "at": "",
+              "clt": "/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" Stepper \")]/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" buttons \")][1]/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" button \")]",
+              "ro": "//*[2]/*[2]/*[local-name()=\"button\"]"
+            }
+        })
+    }
+  }
+
+  browser_step {
+    name = "Click on button \"Next\""
+    type = "click"
+    params {
+      element = jsonencode({
+            "url": "https://wam.playbypoint.com/book/wam",
+            "targetOuterHTML": "<button class=\"ui icon small button primary \"><span class=\"\"> Next <i class=\"icon icon-arrow-right\"></i></span></button>",
+            "multiLocator": {
+              "ab": "/*[local-name()=\"html\"][1]/*[local-name()=\"body\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][2]/*[local-name()=\"table\"][1]/*[local-name()=\"tbody\"][1]/*[local-name()=\"tr\"][1]/*[local-name()=\"td\"][2]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][1]/*[local-name()=\"button\"][1]",
+              "co": "[{\"relation\":\"PARENT OF\",\"tagName\":\"button\",\"text\":\" next \",\"textType\":\"innerText\"},{\"relation\":\"BEFORE\",\"tagName\":\"DIV\",\"text\":\"number of players1234select psphil stephenson4 next \",\"textType\":\"innerText\"}]",
+              "cl": "/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" visible \")]/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" button \") and contains(concat(' ', normalize-space(@class), ' '), \" small \")]",
+              "at": "",
+              "clt": "/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" visible \")]/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" button \") and contains(concat(' ', normalize-space(@class), ' '), \" small \")]",
+              "ro": "//*[2]/*[local-name()=\"div\"][2]/*[1]/*[local-name()=\"button\"][1]"
+            }
+        })
+    }
+  }
+
+  browser_step {
+    name = "Click on button \"Book\""
+    type = "click"
+    params {
+      element = jsonencode({
+            "url": "https://wam.playbypoint.com/book/wam",
+            "targetOuterHTML": "<button class=\"ui button primary fluid  large \">Book</button>",
+            "multiLocator": {
+              "ab": "/*[local-name()=\"html\"][1]/*[local-name()=\"body\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][2]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][3]/*[local-name()=\"div\"][2]/*[local-name()=\"table\"][1]/*[local-name()=\"tbody\"][1]/*[local-name()=\"tr\"][1]/*[local-name()=\"td\"][2]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][4]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"div\"][1]/*[local-name()=\"button\"][1]",
+              "co": "[{\"text\":\"book\",\"textType\":\"directText\"},{\"relation\":\"PARENT OF\",\"tagName\":\"DIV\",\"text\":\"select date and timetuesday, jan 31, 6:30 am - 7:45 am\",\"textType\":\"innerText\"}]",
+              "cl": "/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" button \") and contains(concat(' ', normalize-space(@class), ' '), \" large \")]",
+              "at": "",
+              "clt": "/descendant::*[contains(concat(' ', normalize-space(@class), ' '), \" button \") and contains(concat(' ', normalize-space(@class), ' '), \" large \")]",
+              "ro": "//*[contains(concat(' ', normalize-space(@class), ' '), \" ui \") and contains(concat(' ', normalize-space(@class), ' '), \" button \") and contains(concat(' ', normalize-space(@class), ' '), \" primary \") and contains(concat(' ', normalize-space(@class), ' '), \" fluid \") and contains(concat(' ', normalize-space(@class), ' '), \" large \")]"
+            }
+        })
+    }
+  }
 }
